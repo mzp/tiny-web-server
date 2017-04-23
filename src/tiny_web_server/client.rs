a50cd1f667d426ab77598ca451e57534ccbcec88
mzp <mzpppp@gmail.com>
2017-04-23 13:25:14 +0900

    [from now] 2017/04/23 13:25:14

--- src/tiny_web_server/client.rs
+++ src/tiny_web_server/client.rs
@@ -1,8 +1,20 @@
 use std::net::TcpStream;
-use  std::io::Read;
+use std::io::{Read, Error};
+use std::result::{Result};
 
-pub fn handle_client(mut stream : TcpStream) {
+fn pred(ref c : &Result<u8, Error>) -> bool {
+    c.map(|ch| ch != ('\n' as u8))
+     .unwrap_or(false)
+}
+
+fn read_header(stream : TcpStream) -> String {
     let mut buffer = String::new();
-    stream.read_to_string(&mut buffer);
-    println!("{}", buffer);
+    for s in stream.bytes().take_while(|ch| pred(ch)) {
+        buffer.push(char::from(s.unwrap()));
+    }
+    return buffer;
+}
+
+pub fn handle_client(stream : TcpStream) {
+    read_header(stream);
 }
