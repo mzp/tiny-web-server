c4bb323e4fefb6117a7a4359e8327e663210104d
mzp <mzpppp@gmail.com>
2017-04-23 13:25:14 +0900

    :sparkles: add header reader

--- src/tiny_web_server/client.rs
+++ src/tiny_web_server/client.rs
@@ -1,8 +1,23 @@
 use std::net::TcpStream;
-use  std::io::Read;
+use std::io::{Read, Error};
+use std::result::{Result};
 
-pub fn handle_client(mut stream : TcpStream) {
+fn pred(c : &Result<u8, Error>) -> bool {
+    match c {
+        &Ok(ch) => { ch != ('\n' as u8) }
+        &Err(_) => { false }
+    }
+}
+
+fn read_header(stream : TcpStream) -> String {
     let mut buffer = String::new();
-    stream.read_to_string(&mut buffer);
-    println!("{}", buffer);
+    for s in stream.bytes().take_while(|ch| pred(ch)) {
+        buffer.push(char::from(s.unwrap()));
+    }
+    return buffer;
+}
+
+pub fn handle_client(stream : TcpStream) {
+    let header = read_header(stream);
+    println!("{}", header);
 }
